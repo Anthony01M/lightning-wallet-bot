27625098c9f5069345e8723b53a2f56011454c39
Anthony01M <anthony19990maalouf@gmail.com>
2025-09-09 09:20:17 +0000

    chore(crontab): define discord client.

--- src/crontab.ts
+++ src/crontab.ts
@@ -4,10 +4,13 @@ import db from "@/globals/database"
 import cache from "@/globals/cache"
 import env from "@/globals/env"
 
+import { client } from "@/discord"
+
 export const runContext = {
 	env,
-	database: db,
 	cache,
+	client,
+	database: db,
 	appVersion: getVersion(),
 	join(...strings: (string | number | undefined | null | boolean)[]): string {
 		return strings.filter((str) => str === '' || Boolean(str)).join('\n')
@@ -17,10 +20,12 @@ export const runContext = {
 export default class Builder<Excluded extends (keyof Builder)[] = []> {
 	protected interval: string = '* * * * * *'
 	protected listener: (ctx: typeof runContext) => any | Promise<any> = () => undefined
+	
 	public cron(interval: string): Omit<Builder<[...Excluded, 'cron']>, 'cron' | Excluded[number]> {
 		this.interval = interval
 		return this as any
 	}
+
 	public listen(callback: (ctx: typeof runContext) => any | Promise<any>): Omit<Builder<[...Excluded, 'listen']>, 'listen' | Excluded[number]> {
 		this.listener = callback as any
 		return this as any
